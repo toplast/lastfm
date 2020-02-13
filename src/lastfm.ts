63a4ba7dfd896b307a9d38a1c3a57bd556fe3a80
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-02-13 17:21:43 -0300

    feature: adding tag to lastfm

--- src/lastfm.ts
+++ src/lastfm.ts
@@ -1,11 +1,14 @@
+import { Tag } from "./modules/tag/tag.service";
 import { User } from "./modules/user/user.service";
 
 export class LastFm {
   public readonly user: User
+  public readonly tag: Tag
   private readonly BASE_URL = "http://ws.audioscrobbler.com/2.0";
 
   constructor(API_KEY: string) {
 
     this.user = new User(this.BASE_URL, API_KEY);
+    this.tag = new Tag(this.BASE_URL, API_KEY);
   }
 }
