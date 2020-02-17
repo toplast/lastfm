6234d1d28b4ebe14541b377a73ba127a2c1ac913
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2020-02-17 15:08:29 -0300

    feature: adding tests to last.fm main class

---
+++ test/lastFm.test.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { LastFm } from "../src/lastfm";
+
+describe("Last.fm tests", () => {
+  describe("Constructor test", () => {
+    it("Should set API_KEY", () => {
+      const lastFm = new LastFm("SOME_NICE_API_KEY");
+
+      expect((lastFm as any).API_KEY).toBe("SOME_NICE_API_KEY");
+    });
+
+    it("Should throws error if API_KEY is not set", () => {
+      // @ts-ignore
+      expect(() => new LastFm()).toThrowError(new Error("API key has not set"));
+    });
+  });
+});
